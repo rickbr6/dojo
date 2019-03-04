149114fd2b766e4f516b77f4fd2b8bf940115cc5
Rick Brown <rickbr6@comcast.net>
2019-03-03 23:08:27 -0800

    Updated Sakila assignment

--- MySQL/Sakila Assignment.sql
+++ MySQL/Sakila Assignment.sql
@@ -10,21 +10,17 @@ SELECT customer.first_name,
        city.city_id,
        city.country_id
 FROM customer
-JOIN address
-	on customer.address_id = address.address_id
-JOIN city 
-	on address.city_id = city.city_id
-WHERE
-	city.city_id = 312;
-
+JOIN address on customer.address_id = address.address_id
+JOIN city on address.city_id = city.city_id
+WHERE city.city_id = 312;
 
 -- 2)
 SELECT film.title, 
-	film.description, 
-	film.release_year, 
-	film.rating, 
-	film.special_features,
-	category.name
+    film.description, 
+    film.release_year, 
+    film.rating, 
+    film.special_features,
+    category.name
 FROM film
 JOIN film_category on film.film_id = film_category.film_id
 JOIN category on  film_category.category_id = category.category_id
@@ -33,7 +29,7 @@ WHERE category.category_id = 5;
 -- 3)
 USE sakila;
 SELECT film.title,
-	film.description,
+    film.description,
     film.release_year,
     actor.first_name,
     actor.actor_id,
@@ -45,9 +41,9 @@ WHERE actor.actor_id = 5;
 
 -- 4)
 SELECT customer.first_name,
-	customer.last_name,
+    customer.last_name,
     customer.email,
-	address.address,
+    address.address,
     address.address2,
     address.postal_code,
     city.city  
@@ -58,13 +54,23 @@ WHERE customer.store_id = 1 and
 	address.city_id in (1, 42, 312, 459);
 
 -- 5)
-SELECT film.title, film.description, film.release_year, film.rating, film.special_features FROM film
+SELECT film.title,
+    film.description,
+    film.release_year,
+    film.rating,
+    film.special_features
+FROM film
 JOIN film_actor on film.film_id = film_actor.film_id
 WHERE film_actor.actor_id = 15 and film.rating = "G" and film.special_features LIKE '%behind%';
 
 
 -- 6)
-SELECT film.film_id, film.title, film.description, actor.actor_id, actor.first_name AS actor_first_name, actor.last_name AS actor_last_name
+SELECT film.film_id,
+    film.title,
+    film.description,
+    actor.actor_id,
+    actor.first_name AS actor_first_name,
+    actor.last_name AS actor_last_name
 FROM film
 JOIN film_actor on film.film_id = film_actor.film_id
 JOIN actor on film_actor.actor_id = actor.actor_id
@@ -72,22 +78,20 @@ WHERE film.film_id = 369;
 
 -- 7)
 SELECT film.title,
-	film.description,
+    film.description,
     film.release_year,
     film.rating,
     film.special_features,
     film.rental_rate,
     category.name
 FROM film
-JOIN 
-	film_category on film.film_id = film_category.film_id
-JOIN 
-	category on film_category.category_id = category.category_id
+JOIN film_category on film.film_id = film_category.film_id
+JOIN category on film_category.category_id = category.category_id
 WHERE film.rental_rate = 2.99 and category.category_id = 7;
 
 -- 8)
 SELECT film.title,
-	film.description,
+    film.description,
     film.release_year,
     film.rating,
     film.special_features,
