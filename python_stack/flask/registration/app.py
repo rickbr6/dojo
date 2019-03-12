847f6e4c5ce1af0be28c1123c369a562b58b7ccf
Rick Brown <rickbr6@comcast.net>
2019-03-12 11:50:57 -0700

    Added email validation

--- python_stack/flask/registration/app.py
+++ python_stack/flask/registration/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, redirect, request, session, flash, url_for
 from mysqlconnection import connectToMySQL
+import re
 
 
 app = Flask(__name__)
 app.secret_key = "The super secret key. Don't tell anybody!"
 
 SCHEMA = "registration"
+EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
 
 
 @app.route('/')
@@ -26,6 +28,10 @@ def process():
         is_valid = False
         flash("Please enter a valid last name")
 
+    if not EMAIL_REGEX.match(request.form['email']):
+        is_valid = False
+        flash("Please enter a valid email address")
+
     if len(request.form['password']) < 1:
         is_valid = False
         flash("Please enter a valid password")
@@ -38,13 +44,15 @@ def process():
         print("No errors. Adding user to db")
 
         mysql = connectToMySQL(SCHEMA)
-        query = "INSERT INTO users (first_name, last_name, password, created_at, updated_at) VALUES (%(fname)s, %(lname)s, %(pwd)s, NOW(), NOW());"
+        query = "INSERT INTO users (first_name, last_name, password, created_at, updated_at, email) VALUES (%(fname)s, %(lname)s, %(pwd)s, NOW(), NOW(), %{email));"
         data = {
             'fname': request.form['first_name'],
             'lname': request.form['last_name'],
+            'email': request.form['email'],
             'pwd': request.form['password']
         }
         user_id = mysql.query_db(query, data)
+        flash("Registration successful!")
 
     return redirect(url_for('index'))
 
