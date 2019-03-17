91d1ae68cb8339a1d60925790cbd5d7ada73f571
Rick Brown <rickbr6@comcast.net>
2019-03-16 22:23:54 -0700

    Completed first_orm_app

--- python_stack/flask/first_orm_app/app.py
+++ python_stack/flask/first_orm_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import func
@@ -20,11 +20,30 @@ class Users(db.Model):
     create_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
+    def __repr__(self):
+        return "Users(first_name='%s', last_name='%s', age='%s', email='%s, create_at='%s', updated_at='%s' )"\
+               % (self.first_name, self.last_name, self.age, self.email, self.create_at, self.updated_at)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+
+    all_users = Users.query.all()
+    print(all_users)
+
+    return render_template("index.html", users=all_users)
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    print(f"REQUEST: {request.form['first_name']}")
+
+    new_user = Users(first_name=request.form['first_name'], last_name=request.form['last_name'],
+                     email=request.form['email'])
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/")
 
 
 if __name__ == '__main__':
